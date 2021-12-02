6fc002a49c7bf7ebfc35f5256d674d611a702b36
vvicunag <vvicuna1@uc.cl>
2021-12-02 16:23:44 -0300

    add note rendering via get request of json file

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const fs = require("fs"); 
 const path = require('path');
+const notes = require("./db/db.json")
 
 const PORT = 3001; 
 
@@ -12,16 +13,48 @@ app.use(express.static("public"));
 
 
 // GET Route for homepage
-app.get('/', (req, res) =>
-  res.sendFile(path.join(__dirname, '/public/index.html'))
-);
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '/public/index.html'));
+  console.info("received request to render index html")
+});
 
-// GET Route for /notes
-app.get('/notes', (req, res) =>
-  res.sendFile(path.join(__dirname, '/public/notes.html'))
-);
+// GET Route for /notes html
+app.get('/notes', (req, res) => {
+  res.sendFile(path.join(__dirname, '/public/notes.html'));
+});
+
+//GET request for notes
+app.get('/api/notes', (req, res) => {
+  res.status(200).json(notes);
+});
+
 
 
 app.listen(PORT, () =>
   console.log(`App listening at http://localhost:${PORT}`)
 );
+
+
+
+// app.post('/api/tips', (req, res) => {
+//   console.info(`${req.method} request received to add a tip`);
+
+//   const { username, topic, tip } = req.body;
+
+//   if (req.body) {
+//     const newTip = {
+//       username,
+//       tip,
+//       topic,
+//       tip_id: uuid(),
+//     };
+
+//     readAndAppend(newTip, './db/tips.json');
+//     res.json(`Tip added successfully 🚀`);
+//   } else {
+//     res.error('Error in adding tip');
+//   }
+// });
+
+
+
